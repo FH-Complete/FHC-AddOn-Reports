d93bb5a5ad99403ae1ce7cfb47e9a0b4ca31762a
Andreas Moik <moik@technikum-wien.at>
2016-09-20 15:14:50 +0200

    bugfix

--- cis/vorschau.php
+++ cis/vorschau.php
@@ -64,7 +64,7 @@
 	?>
 
 
-		<div id="spinner" style="display:none; width:80%; margin-left:10%; top:50px; position:absolute; z-index:10;">
+		<div id="spinner" style="display:none; width:80%; margin-left:10%; top:30px; position:absolute; z-index:10;">
 			<div class="progress">
 					<div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
 					Loading Data
