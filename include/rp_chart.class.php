88e1b71cd2adccef4159c3c4b376d310674949a4
Andreas Moik <moik@technikum-wien.at>
2016-10-10 16:43:57 +0200

    bugfix for pre php 5.4

--- include/rp_chart.class.php
+++ include/rp_chart.class.php
@@ -843,7 +843,7 @@ EOT;
 				'labels' => array('rotation' => -45),
 				'categories' => array(),
 			);
-			$colors = ['#7cb5ec', '#434348', '#90ed7d', '#f7a35c', '#8085e9', '#f15c80', '#e4d354', '#2b908f', '#f45b5b', '#91e8e1'];
+			$colors = array('#7cb5ec', '#434348', '#90ed7d', '#f7a35c', '#8085e9', '#f15c80', '#e4d354', '#2b908f', '#f45b5b', '#91e8e1');
 			$colorCount = 0;
 
 			$categories = array();
