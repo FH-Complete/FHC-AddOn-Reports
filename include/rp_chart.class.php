8fce1ac7680cfe6390061276b1d5d2711e463549
Andreas Moik <moik@technikum-wien.at>
2016-10-13 13:40:22 +0200

    added more colors to grouped-stacked

--- include/rp_chart.class.php
+++ include/rp_chart.class.php
@@ -845,7 +845,7 @@ EOT;
 				'labels' => array('rotation' => -45),
 				'categories' => array(),
 			);
-			$colors = array('#7cb5ec', '#434348', '#90ed7d', '#f7a35c', '#8085e9', '#f15c80', '#e4d354', '#2b908f', '#f45b5b', '#91e8e1');
+			$colors = array('#7cb5ec', '#434348', '#90ed7d', '#f7a35c', '#8085e9', '#f15c80', '#e4d354', '#2b908f', '#f45b5b', '#91e8e1', '#BBBBBB', '#992222', '#226622', '#222266', '#EEAAAA', '#FFFFBB', '#FFBBFF');
 			$colorCount = 0;
 
 			$categories = array();
